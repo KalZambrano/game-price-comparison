241e25086ecc27e5ac27dc58efc306e097173345
Kaleb <kalebzambrano@outlook.com>
2026-01-06 16:43:12 -0500

    cambiando tailwind y url de componentes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      animation: {
-        'spin': 'spin 1s linear infinite',
-      }
-    },
-  },
-  plugins: [],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
 }
